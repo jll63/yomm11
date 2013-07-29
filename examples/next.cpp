fb819d5b61e48af62606ef43b6a7aa5d8cc8ed69
Jean-Louis Leroy <jl@yorel.be>
2013-07-29 19:24:53 +0100

    Improve next.cpp.

--- examples/next.cpp
+++ examples/next.cpp
@@ -12,17 +12,20 @@
 #include <multimethods.hpp>
 
 using namespace std;
+using multimethods::selector;
 using multimethods::virtual_;
 
-struct Vehicle : multimethods::selector {
-  MM_CLASS(Vehicle);
+// Vehicle is the root of a class hierarchy that includes support for fast dispatch.
+// That means:
+struct Vehicle : selector { // inherit from class selector
+  MM_CLASS(Vehicle); // register class
   Vehicle() {
-    MM_INIT();
+    MM_INIT(); // initialize pointer to multi-method table
   }
 };
 
 struct Car : Vehicle {
-  MM_CLASS(Car, Vehicle);
+  MM_CLASS(Car, Vehicle); // register class and its base
   Car() {
     MM_INIT();
   }
@@ -49,14 +52,18 @@ struct StateInspector : Inspector {
   }
 };
 
+// declare a multi-method with two virtual arguments
+// this can go in a header file
 MULTIMETHOD(inspect, void(virtual_<Vehicle>&, virtual_<Inspector>&));
 
+// define a specialization
+// this must go in an implementation file
 BEGIN_METHOD(inspect, void, Vehicle& v, Inspector& i) {
   cout << "Inspect vehicle.\n";
 } END_METHOD;
 
 BEGIN_METHOD(inspect, void, Car& v, Inspector& i) {
-  next(v, i);
+  next(v, i); // next calls the next most specialized method
   cout << "Inspect seat belts.\n";
 } END_METHOD;
 
@@ -66,9 +73,10 @@ BEGIN_METHOD(inspect, void, Car& v, StateInspector& i) {
 } END_METHOD;
 
 int main() {
-  multimethods::initialize(); // IMPORTANT!
+  multimethods::initialize(); // IMPORTANT! - allocates slots and compute dispatch tables
   Car car;
   StateInspector inspector;
+  // call method:
   inspect(car, inspector); // Inspect vehicle. Inspect seat belts. Check insurance.
   return 0;
 }
